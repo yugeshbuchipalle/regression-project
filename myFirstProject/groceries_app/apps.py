8bdec08f35d2710b6cf9f4fa4d9feededc3344b9
yugeshbuchipalle <69415092+yugeshbuchipalle@users.noreply.github.com>
2024-04-19 13:11:23 +0530

    Delete myFirstProject directory

--- myFirstProject/groceries_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroceriesAppConfig(AppConfig):
-    name = 'groceries_app'
--- myFirstProject/products_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class OrderList(models.Model):
-    username = models.CharField(max_length=200)
-    wholelist = models.CharField(max_length=500,blank=True,null=True)
